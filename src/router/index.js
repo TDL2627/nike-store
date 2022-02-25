e70d91a9f22876ebae2201ef45ee23dd232b1623
TDL2627 <the.dark.lord.2627@gmail.com>
2022-02-25 09:19:31 +0200

    added cart view

--- src/router/index.js
+++ src/router/index.js
@@ -43,6 +43,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Products.vue"),
   },
+  {
+    path: "/cart",
+    name: "Cart",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Cart.vue"),
+  }
 ]
 
 const router = createRouter({
